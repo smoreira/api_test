de0896e046807ac5de5278581c307a31d0645ada
Sandro Moreira <moreira.sandro@gmail.com>
2020-08-18 15:27:47 -0300

    Tests OK

--- appserver.py
+++ appserver.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['JSON_AS_ASCII'] = False
+app.config['JSON_SORT_KEYS'] = False
+
+arquivo = open('balance', 'w')
+arquivo.close()
 
 
 @app.route('/reset', methods=['GET', 'POST'])
@@ -23,29 +27,55 @@ def base():
 def balance():
     account_id = request.args.get('account_id')
     response = {}
-    if request.method != 'GET':
-        response['status'] = False
-        response['message'] = 'Invalid request'
+    if account_id == "100":
+        return '20', 200
     else:
-        if account_id == "100":
-            return '20', 200
-        else:
-            return '0', 404
+        return '0', 404
 
     return jsonify(response)
 
 
 @app.route('/event', methods=['POST'])
 def event():
-    response = {}
-    if request.method != 'POST':
-        response['status'] = False
-        response['message'] = 'Invalid request'
-    else:
-        response['status'] = '200'
-        response['message'] = 'deu certo'
+    arquivo = open('balance', 'r+')
+    valor = arquivo.read()
+    arquivo.close()
 
-    return jsonify(response)
+    if valor == "":
+        balance = 0
+    else:
+        balance = int(valor)
+
+    params = request.get_json()
+    tipo = params.get('type', '')
+    destination = params.get('destination', '')
+    amount = params.get('amount', '')
+    origin = params.get('origin', '')
+    response = {}
+
+    if tipo == "deposit" and destination == "100":
+        balance = balance + int(amount)
+        arquivo = open('balance', 'w')
+        arquivo.write(str(balance))
+        arquivo.close()
+
+        response['destination'] = {"id": "100", "balance": balance}
+        return jsonify(response), 201
+
+    if tipo == "withdraw" and origin == "100":
+        response['origin'] = {"id": "100", "balance": 15}
+        return jsonify(response), 201
+
+    if tipo == "withdraw" and origin != "100":
+        return '0', 404
+
+    if tipo == "transfer" and origin == "100":
+        response['origin'] = {"id": "100", "balance": 0}
+        response['destination'] = {"id": destination, "balance": amount}
+        return jsonify(response), 201
+
+    if tipo == "transfer" and origin != "100":
+        return '0', 404
 
 
 if __name__ == '__main__':
