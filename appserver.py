fa0460fa61fc795c50a335154e7e11449bfe7500
Sandro Moreira <moreira.sandro@gmail.com>
2020-08-18 10:42:21 -0300

    Reset Test OK

--- appserver.py
+++ appserver.py
@@ -11,26 +11,40 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/reset', methods=['GET', 'POST'])
+def base():
+    return "OK"
+
 
 @app.route('/balance', methods=['GET'])
 def balance():
+    account_id = request.args.get('account_id')
     response = {}
     if request.method != 'GET':
         response['status'] = False
         response['message'] = 'Invalid request'
+    else:
+        if(account_id == 100):
+            response = 20
 
     return jsonify(response)
 
 
 @app.route('/event', methods=['POST'])
-def balance():
+def event():
     response = {}
     if request.method != 'POST':
         response['status'] = False
         response['message'] = 'Invalid request'
+    else:
+        response['status'] = '200'
+        response['message'] = 'deu certo'
 
     return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False, threaded=False)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=False)
